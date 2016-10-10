99710d7295c655c274d5a96955965fa62918c5a0
JakeDrohman <jkdrohman@gmail.com>
2016-10-10 07:37:22 -0500

    created bst project

---
+++ BinarySearchTreeProject/BinarySearchTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProject
+{
+    class BinarySearchTree
+    {
+        private Node baseNode;
+        private int count;
+
+        public BinarySearchTree(int rootvalue)
+        {
+            baseNode = new Node(rootvalue);
+            count = 1;
+        }
+        public void InsertValue(int value)
+        {
+            baseNode.SetValue(value);
+            count++;
+        }
+        public bool Search(int value)
+        {
+            return baseNode.Search(value);
+        }
+    }
+}
---
+++ BinarySearchTreeProject/Node.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProject
+{
+    class Node
+    {
+        public Node RightNode;
+        public Node LeftNode;
+        private int value;
+
+        public Node(int input)
+        {
+            value = input;
+        }
+        public void SetValue(int value)
+        {
+            if (this.value > value)
+            {
+                if(LeftNode == null)
+                {
+                    LeftNode = new Node(value);
+                }
+                else
+                {
+                    LeftNode.SetValue(value);
+                }
+            }
+            else if(this.value < value)
+            {
+                if (RightNode == null)
+                {
+                    RightNode = new Node(value);
+                }
+                else
+                {
+                    RightNode.SetValue(value);
+                }
+            }
+        }
+        public bool Search(int value)
+        {
+            if(value == this.value)
+            {
+                return true;
+            }
+            else if (value > this.value)
+            {
+                return RightNode.Search(value);
+            }
+            else if (value < this.value)
+            {
+                return LeftNode.Search(value);
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
